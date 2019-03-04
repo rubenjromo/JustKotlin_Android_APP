fceddf2081d399440e569517a6cb081d5082289d
RubenJromo <rjromo@espol.edu.ec>
2019-03-04 18:48:08 -0500

    Deleted unnecesary functions and admob test banner ad added

--- app/src/main/java/com/firstapp/rjromo/justkotlin/MainActivity.kt
+++ app/src/main/java/com/firstapp/rjromo/justkotlin/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 import java.text.NumberFormat
 
 
@@ -15,12 +16,11 @@ import java.text.NumberFormat
  */
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdView : AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        lateinit var mAdView : AdView
-
 
         MobileAds.initialize(this, "ca-app-pub-4597919722020460~5704079985")
 
@@ -35,28 +35,30 @@ class MainActivity : AppCompatActivity() {
 
     var quantity: Int = 0
 
-    private fun calculatePrice(view: View):Int{
-        var price = quantity * 5
-        return price
-    }
 
     fun increment(view: View) {
         quantity++
         display(quantity)
-        displayPrice(calculatePrice())
     }
 
     fun decrement(view: View) {
         quantity--
         display(quantity)
-        displayPrice(calculatePrice())
+    }
+
+    /**
+     * This method calculate price.
+     */
+    private fun calculatePrice():Int{
+        return quantity * 5
+
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     fun submitOrder(view: View) {
-        val message: String = "Total: ${NumberFormat.getCurrencyInstance().format(calculatePrice())} \nThank you"
+        val message: String = createOrderSummary()
         displayMessage(message)
     }
 
@@ -70,19 +72,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * This method displays the given price on the screen.
-     */
-    private fun displayPrice(number: Int) {
-        val priceTextView = findViewById<View>(R.id.price_text_view) as TextView
-        priceTextView.text = NumberFormat.getCurrencyInstance().format(number)
-    }
-
-    /**
      * This method displays a message on the screen.
      */
     private fun displayMessage(message: String) {
         val priceTextView = findViewById<View>(R.id.price_text_view) as TextView
         priceTextView.text = message
+    }
+
+    fun createOrderSummary():String {
+        var priceMessage:String = "Rubén Romo \nQuantity: ${quantity} \nTotal: ${NumberFormat.getCurrencyInstance().format(calculatePrice())} \nThank you!"
+        return priceMessage
     }
 
 
